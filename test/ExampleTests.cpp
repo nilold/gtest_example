5262b61d673f2e6775f1341f2dfb27e5a256f99c
Nilo Neto <nilo@global.cadence.com>
2020-07-03 16:49:03 -0300

    gTestExample

---
+++ test/ExampleTests.cpp
@@ -0,0 +1,44 @@
+#include "../Example.hpp"
+#include <gtest/gtest.h>
+
+struct ExampleTests 
+    : public ::testing::Test
+{
+    int* x;
+
+    virtual void SetUp() override {
+        x = new int{42};
+    }
+
+    virtual void TearDown() override {
+        delete x;
+    }
+
+    int getX() {
+        return *x;
+    }
+};
+
+TEST(ExampleTests, DemoGTestMacros) {
+    EXPECT_TRUE(true);
+    ASSERT_TRUE(true); // stops if  fails
+    EXPECT_EQ(65, 'A') << "This will be printed if its fails";
+}
+
+TEST(Exampletests, MAC) {
+    int x = 42;
+    int y = 16;
+    int sum = 100;
+    int oldSum = sum;
+    int expectedNewSum = oldSum + x * y;
+
+    EXPECT_EQ(
+        expectedNewSum,
+        MAC(x, y, sum)
+    );
+
+    EXPECT_EQ(
+        expectedNewSum,
+        sum
+    );
+}
